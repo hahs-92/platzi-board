d8bd594103b358c6b8851d79b7b5cd3f88b0a916
hahs <alexandre_2420@outlook.com>
2022-09-28 14:26:27 -0500

    implement mergeAll

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,39 @@
-import { fromEvent } from "rxjs";
-import { map } from "rxjs/operators";
+import { fromEvent, map, mergeAll } from "rxjs";
 import "./style.css";
 
 const canvas: HTMLCanvasElement = document.querySelector(".canvas")!;
-const cursorPosition = {
-  x: 0,
-  y: 0,
-};
-
-const normalizePosition = (canvas: HTMLCanvasElement, event: MouseEvent) => {
-  cursorPosition.x = event.clientX - canvas.offsetLeft;
-  cursorPosition.y = event.clientY - canvas.offsetTop;
-};
-
-const onMouseDown$ = fromEvent(canvas, "mousedown").pipe(
-  map((event) => {
-    //restamos la diferente del canvas con la pantalla completa
-    normalizePosition(canvas, event as MouseEvent);
-    console.log(cursorPosition);
-  })
-);
-const onMouseMove$ = fromEvent(canvas, "mousemove");
-const onMouseUp$ = fromEvent(canvas, "mouseup");
-
-onMouseDown$.subscribe(console.log);
+const cursorPosition = { x: 0, y: 0 };
 
 //
 const canvasContext: CanvasRenderingContext2D = canvas.getContext("2d")!;
 canvasContext.lineWidth = 8;
 canvasContext.strokeStyle = "white";
 
-//realizar trazo
-canvasContext.beginPath();
-canvasContext.moveTo(50, 0);
-canvasContext.lineTo(100, 100);
-canvasContext.stroke();
-canvasContext.closePath();
+const updateCursorPosition = (canvas: HTMLCanvasElement, event: MouseEvent) => {
+  //restamos la diferente del canvas con la pantalla completa
+  cursorPosition.x = event.clientX - canvas.offsetLeft;
+  cursorPosition.y = event.clientY - canvas.offsetTop;
+};
+
+const paintStroke = (event: MouseEvent) => {
+  //realizar trazo
+  canvasContext.beginPath();
+  canvasContext.moveTo(cursorPosition.x, cursorPosition.y);
+  updateCursorPosition(canvas, event);
+  canvasContext.lineTo(cursorPosition.x, cursorPosition.y);
+  canvasContext.stroke();
+  canvasContext.closePath();
+};
+
+const onMouseDown$ = fromEvent(canvas, "mousedown");
+const onMouseMove$ = fromEvent(canvas, "mousemove");
+const onMouseUp$ = fromEvent(canvas, "mouseup");
+
+onMouseDown$.subscribe((e) => updateCursorPosition(canvas, e as MouseEvent));
+
+const startPaint$ = onMouseDown$.pipe(
+  map(() => onMouseMove$),
+  mergeAll()
+);
+
+startPaint$.subscribe((e) => paintStroke(e as MouseEvent));
